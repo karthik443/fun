d7bcd2cb5b91e3fbdf16e16536a490d96ff6b494
karthik <karthiknallala31@gmail.com>
2024-02-05 21:11:56 +0530

    yes

--- one.java
+++ one.java
@@ -2,7 +2,9 @@ import java.util.*;
 class one{
     public static void main(String[] args) {
       int arr[]= {4,2,1,5,3,22,8};
-      System.out.println("hello world");
+      for(int i=0;i<arr.length;i++){
+        System.out.println("hello world this is karthik ");
+      }
 
     }
 }
